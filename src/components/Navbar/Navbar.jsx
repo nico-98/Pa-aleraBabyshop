d0ab65e4418df8415a169e6bed9b0541f57f6f9f
Nicolas Plana <nikolasplana@gmail.com>
2025-03-05 19:01:41 -0300

    Inicio proyecto con Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import './Navbar.css'
+
+function Navbar() {
+return (
+    <header>
+        <nav Class="nav-bar">
+            <p>Logo</p>
+        <ul Class="nav-bar-items">
+            <li>Inicio</li>
+            <li>Productos</li>
+            <li>Contacto</li>
+        </ul>
+        <p>Carrito</p>
+        </nav>
+    </header>
+    )
+}
+export default Navbar
