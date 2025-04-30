3a8141b04184d7a2880ace0fa5fd6ef32eff7d70
Nicolas Plana <nikolasplana@gmail.com>
2025-04-30 00:29:22 -0300

    correccion de nombre de logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import CartWidget from '../CartWidget/CartWidget'
 import './Navbar.css'
-import logo from '../../assets/logo.PNG';
+import logo from '../../assets/logo.png';
 import { Link } from 'react-router-dom'
 
 function Navbar() {
